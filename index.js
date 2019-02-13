243246595ed6b78425117c26f166ddfd125b9462
ptitsynamasha <ptitsynamv@gmail.com>
2019-02-13 15:09:48 +0200

    first

--- index.js
+++ index.js
@@ -3,7 +3,32 @@ const fs = require('fs');
 const mkdirp = require('mkdirp');
 const path = require('path');
 const DomParser = require('dom-parser');
+const async = require('async');
 const parser = new DomParser();
+
+const urls = [
+    'http://antropogenez.ru/articles/p/1',
+    'http://antropogenez.ru/articles/p/2',
+    'http://antropogenez.ru/articles/p/3',
+    'http://antropogenez.ru/articles/p/4',
+    'http://antropogenez.ru/articles/p/5',
+    'http://antropogenez.ru/articles/p/6',
+    'http://antropogenez.ru/articles/p/8',
+    // 'http://antropogenez.ru/articles/p/9',
+    // 'http://antropogenez.ru/articles/p/10',
+    // 'http://antropogenez.ru/articles/p/11',
+    // 'http://antropogenez.ru/articles/p/12',
+    // 'http://antropogenez.ru/articles/p/13',
+    // 'http://antropogenez.ru/articles/p/14',
+    // 'http://antropogenez.ru/articles/p/15',
+    // 'http://antropogenez.ru/articles/p/16',
+    // 'http://antropogenez.ru/articles/p/17',
+    // 'http://antropogenez.ru/articles/p/18',
+    // 'http://antropogenez.ru/articles/p/19',
+    // 'http://antropogenez.ru/articles/p/20',
+    // 'http://antropogenez.ru/articles/p/21',
+];
+
 
 function getArticlesData(body) {
     const dom = parser.parseFromString(body, 'text/html');
@@ -54,126 +79,53 @@ function downloadArticle(url, filename, callback) {
             if (err) {
                 return callback(err);
             }
+            console.log(`Save: ${filename}`);
             callback(null, filename);
         })
     })
 }
 
-class TaskQueue {
-    constructor(concurrency) {
-        this.concurrency = concurrency;
-        this.running = 0;
-        this.queue = [];
-    }
-
-    pushTask(task) {
-        this.queue.push(task);
-        this.next();
-    }
-
-    next() {
-        while (this.running < this.concurrency && this.queue.length) {
-            const task = this.queue.shift();
-            task(() => {
-                this.running--;
-                this.next();
-            });
-            this.running++;
-        }
-    }
-}
-
-const downloadQueue = new TaskQueue(2);
-
-function spiderLinks(currentUrl, body, nesting, callback) {
-    if (nesting === 0) {
-        return process.nextTick(callback);
-    }
-    const links = getArticlesData(body);
-    if (links.length === 0) {
-        return process.nextTick(callback);
-    }
-    let completed = 0, hasErrors = false;
-    links.forEach(link => {
-        downloadQueue.pushTask(done => {
-            spider(link, nesting - 1, err => {
+function asyncSaveArticles() {
+    async.eachSeries(urls, (url, mainCallback) => {
+        let articlesData;
+        async.series([
+                callback => {
+                    request(url, (err, resp, bodyPage) => {
+                        if (err || resp.statusCode >= 400) {
+                            return callback(err)
+                        }
+                        articlesData = getArticlesData(bodyPage);
+                        callback(null, url)
+                    });
+                },
+                callback => {
+                    async.eachSeries(articlesData, (link, callback) => {
+                        downloadArticle(link.url, link.subject, callback)
+                    }, (err, result) => {
+                        if (err) {
+                            return callback(err)
+                        }
+                        callback(null, 'All pages');
+                    })
+                }
+            ],
+            (err, result) => {
                 if (err) {
-                    hasErrors = true;
-                    return callback(err);
+                    return mainCallback(err)
                 }
-                if (++completed === links.length && !hasErrors) {
-                    callback(null, currentUrl);
-                }
-                done();
+                console.log('series', result);
+                mainCallback(null, result)
             });
-        });
-    });
-
-}
-
-const spideringMap = new Map();
-
-function spider(linkData, nesting, callback) {
-    const filename = linkData.subject ? linkData.subject : linkData;
-    const url = linkData.url ? linkData.url : linkData;
-
-    if (spideringMap.has(url)) {
-        return process.nextTick(callback)
-    }
-    spideringMap.set(url, true);
-
-    request(url, (err, resp, body) => {
-        if (err || resp.statusCode >= 400) {
-            return callback(err)
-        }
-        if (nesting === 0) {
-            console.log('Downloading: ' + filename);
-            return downloadArticle(url, filename, (err, filename) => {
-                if (err) {
-                    return callback(err);
-                }
-                return callback(null);
-            });
-        }
-        spiderLinks(filename, body, nesting, callback);
-    });
-}
-
-
-const urls = [
-    'http://antropogenez.ru/articles/p/1',
-    'http://antropogenez.ru/articles/p/2',
-    'http://antropogenez.ru/articles/p/3',
-    'http://antropogenez.ru/articles/p/4',
-    'http://antropogenez.ru/articles/p/5',
-    'http://antropogenez.ru/articles/p/6',
-    'http://antropogenez.ru/articles/p/8',
-    'http://antropogenez.ru/articles/p/9',
-    'http://antropogenez.ru/articles/p/10',
-    'http://antropogenez.ru/articles/p/11',
-    'http://antropogenez.ru/articles/p/12',
-    'http://antropogenez.ru/articles/p/13',
-    'http://antropogenez.ru/articles/p/14',
-    'http://antropogenez.ru/articles/p/15',
-    'http://antropogenez.ru/articles/p/16',
-    'http://antropogenez.ru/articles/p/17',
-    'http://antropogenez.ru/articles/p/18',
-    'http://antropogenez.ru/articles/p/19',
-    'http://antropogenez.ru/articles/p/20',
-    'http://antropogenez.ru/articles/p/21',
-];
-
-urls.forEach(url => {
-    /*
-    downloading only with nesting = 0
-     */
-    spider(url, 1, (err, filename) => {
+    }, (err, result) => {
         if (err) {
             console.log(err);
         } else {
-            console.log(`"${filename}" was already downloaded`);
+            console.log(`"${result}" was already downloaded`);
         }
     });
-});
+}
+
+asyncSaveArticles();
+
 
 
