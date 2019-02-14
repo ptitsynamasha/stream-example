6d8befc35ea806d3893c4f8f3e9c98ba5ffe80e5
ptitsynamasha <ptitsynamv@gmail.com>
2019-02-14 12:12:06 +0200

    first

--- index.js
+++ index.js
@@ -1,9 +1,61 @@
-const request = require('request');
 const fs = require('fs');
-const mkdirp = require('mkdirp');
 const path = require('path');
 const DomParser = require('dom-parser');
 const parser = new DomParser();
+const functionToPromise = require('./utilities/functionToPromise');
+const request = functionToPromise(require('request'));
+const mkdirp = functionToPromise(require('mkdirp'));
+const writeFile = functionToPromise(fs.writeFile);
+
+
+// 194 piece
+const urls = [
+    'http://antropogenez.ru/articles/p/1',
+    'http://antropogenez.ru/articles/p/2',
+    'http://antropogenez.ru/articles/p/3',
+    'http://antropogenez.ru/articles/p/4',
+    'http://antropogenez.ru/articles/p/5',
+    'http://antropogenez.ru/articles/p/6',
+    'http://antropogenez.ru/articles/p/8',
+    'http://antropogenez.ru/articles/p/9',
+    'http://antropogenez.ru/articles/p/10',
+    'http://antropogenez.ru/articles/p/11',
+    'http://antropogenez.ru/articles/p/12',
+    'http://antropogenez.ru/articles/p/13',
+    'http://antropogenez.ru/articles/p/14',
+    'http://antropogenez.ru/articles/p/15',
+    'http://antropogenez.ru/articles/p/16',
+    'http://antropogenez.ru/articles/p/17',
+    'http://antropogenez.ru/articles/p/18',
+    'http://antropogenez.ru/articles/p/19',
+    'http://antropogenez.ru/articles/p/20',
+    'http://antropogenez.ru/articles/p/21',
+];
+
+class Queue {
+    constructor(maxRunning) {
+        this.maxRunning = maxRunning;
+        this.running = 0;
+        this.queue = [];
+    }
+
+    pushTask(task) {
+        this.queue.push(task);
+        this.next();
+    }
+
+    next() {
+        while (this.running < this.maxRunning && this.queue.length) {
+            const task = this.queue.shift();
+            task()
+                .then(() => {
+                    this.running--;
+                    this.next();
+                });
+            this.running++;
+        }
+    }
+}
 
 function getArticlesData(body) {
     const dom = parser.parseFromString(body, 'text/html');
@@ -35,109 +87,82 @@ function getArticleText(body) {
     return article[0].innerHTML;
 }
 
-function saveFile(filename, contents, callback) {
-    mkdirp(path.dirname(filename), err => {
-        if (err) {
-            return callback(err);
-        }
-        fs.writeFile(filename, contents, callback);
-    });
+function saveFile(filename, contents) {
+    return mkdirp(path.dirname(filename))
+        .then(_ => writeFile(filename, contents));
 }
 
-function downloadArticle(url, filename, callback) {
-    request(url, (err, resp, articleBody) => {
-        if (err || resp.statusCode >= 400) {
-            return callback(err)
-        }
-        const text = getArticleText(articleBody);
-        saveFile(`articles/${filename}.txt`, text, err => {
-            if (err) {
-                return callback(err);
-            }
-            callback(null, filename);
+function downloadArticle(url, filename) {
+    return request(url)
+        .then(({body, statusCode}) => {
+            return getArticleText(body);
         })
-    })
+        .then(text => saveFile(`articles/${filename}.txt`, text))
 }
+
+const queue = new Queue(3);
 
 function spiderLinks(currentUrl, body, nesting) {
     if (nesting === 0) {
         return Promise.resolve();
     }
     const links = getArticlesData(body);
-    const promises = links.map(link => spider(link, nesting - 1, err => {
-        if (err) {
-            return Promise.reject(err)
-        }
-    }));
+    if (links.length === 0) {
+        return Promise.resolve();
+    }
 
-    return Promise.all(promises);
+    return new Promise((resolve, reject) => {
+        let completed = 0;
+        let error = false;
+        links.forEach(link => {
+            const task = () => {
+                return spider(link, nesting - 1)
+                    .then(_ => {
+                        if (++completed === links.length) {
+                            return resolve(currentUrl)
+                        }
+                    })
+                    .catch(err => {
+                        if (!error) {
+                            error = err;
+                            reject(err);
+                        }
+                    });
+            };
+            queue.pushTask(task);
+        })
+    })
 }
 
 const spideringMap = new Map();
 
-function spider(linkData, nesting, callback) {
+function spider(linkData, nesting) {
     const filename = linkData.subject ? linkData.subject : linkData;
     const url = linkData.url ? linkData.url : linkData;
 
     if (spideringMap.has(url)) {
-        return process.nextTick(callback)
+        return Promise.resolve()
     }
     spideringMap.set(url, true);
 
-    request(url, (err, resp, body) => {
-        if (err || resp.statusCode >= 400) {
-            return callback(err)
-        }
-        if (nesting === 0) {
-            console.log('Downloading: ' + filename);
-            return downloadArticle(url, filename, (err, filename) => {
-                if (err) {
-                    return callback(err);
-                }
-                return callback(null);
-            });
-        }
-        spiderLinks(filename, body, nesting)
-            .then(() => callback(null, filename))
-            .catch(err => callback(err))
-    });
+    return request(url)
+        .then(({body, statusCode}) => {
+            if (nesting === 0) {
+                console.log('Downloading: ' + filename);
+                downloadArticle(url, filename);
+            }
+            return body;
+        })
+        .then(body => spiderLinks(filename, body, nesting))
 }
-
-// 194 piece
-const urls = [
-    'http://antropogenez.ru/articles/p/1',
-    'http://antropogenez.ru/articles/p/2',
-    'http://antropogenez.ru/articles/p/3',
-    // 'http://antropogenez.ru/articles/p/4',
-    // 'http://antropogenez.ru/articles/p/5',
-    // 'http://antropogenez.ru/articles/p/6',
-    // 'http://antropogenez.ru/articles/p/8',
-    // 'http://antropogenez.ru/articles/p/9',
-    // 'http://antropogenez.ru/articles/p/10',
-    // 'http://antropogenez.ru/articles/p/11',
-    // 'http://antropogenez.ru/articles/p/12',
-    // 'http://antropogenez.ru/articles/p/13',
-    // 'http://antropogenez.ru/articles/p/14',
-    // 'http://antropogenez.ru/articles/p/15',
-    // 'http://antropogenez.ru/articles/p/16',
-    // 'http://antropogenez.ru/articles/p/17',
-    // 'http://antropogenez.ru/articles/p/18',
-    // 'http://antropogenez.ru/articles/p/19',
-    // 'http://antropogenez.ru/articles/p/20',
-    // 'http://antropogenez.ru/articles/p/21',
-];
 
 urls.forEach(url => {
     /*
     downloading only with nesting = 0
      */
-    spider(url, 1, (err, filename) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(`"${filename}" was already downloaded`);
-        }
-    });
+    spider(url, 1)
+        .then(filename => console.log(`"${filename}" was already downloaded`))
+        .catch(err => console.log(err));
 });
 
 
